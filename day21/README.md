c21cc9220e3665d945a2eb6c4f4120801f013f80
dpnet.ca <git@dpnet.ca>
2021-01-21 12:49:44 -0600

    Day 21 complete

---
+++ day21/README.md
@@ -0,0 +1,3 @@
+# Day 21
+
+Class inheritence and snake game part 2
---
+++ day21/excercise.py
@@ -0,0 +1,24 @@
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
+
+    def breathe(self):
+        print("inhale, exhale")
+
+
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+
+    def swim(self):
+        print("moving in water")
+
+    def breathe(self):
+        super().breathe()
+        print("   ... but under water")
+
+
+nemo = Fish()
+nemo.swim()
+nemo.breathe()
+print(nemo.num_eyes)
